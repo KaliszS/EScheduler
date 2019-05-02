f783836c2e4ed9973729925ac2b0b6dab3cb6750
295803 <295803@ee.pw.edu.pl>
2019-05-02 21:58:48 +0200

    nowe wydarzenie kolejne

--- src/OknoNowegoWydarzenia.java
+++ src/OknoNowegoWydarzenia.java
@@ -2,10 +2,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -26,12 +24,12 @@ public class OknoNowegoWydarzenia {
     @FXML
     private TextField miejsce;
     @FXML
-    private TextArea podsumowanie;
+    private Label podsumowanie;
 
     public void wyswietl(Stage primaryStage) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("OknoNowegoWydarzenia.fxml"));
         Parent root = loader.load();
-        Scene scene = new Scene(root, 600, 400);
+        Scene scene = new Scene(root, 660, 400);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
@@ -47,7 +45,9 @@ public class OknoNowegoWydarzenia {
         {
             return;
         }
-        podsumowanie.setText(nazwa.getText() + "\nPocz: " + rozp + "\nKon: " + zako + "\nMiejsce: " + miejsce.getText() + "\nOpis: " + opis.getText());
+
+        podsumowanie.setText("PODSUMOWANIE\n" + nazwa.getText() + "\nPocz: " + rozp + "\nKon: " +
+                zako + "\nMiejsce: " + miejsce.getText() + "\nOpis: " + opis.getText());
     }
 
 }
